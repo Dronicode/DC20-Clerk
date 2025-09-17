376bca64a6db671f1c2e3a3dfffec623b8e1511d
Dronicode <luffy.obrien@outlook.com>
2025-09-18 00:48:23 +0200

    added logging middleware

--- backend/identity/cmd/main.go
+++ backend/identity/cmd/main.go
@@ -9,10 +9,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("[%s] %s %s", r.Method, r.URL.Path, r.RemoteAddr)
+        next.ServeHTTP(w, r)
+    })
+}
+
 func main() {
   godotenv.Load(".env")
 
   r := mux.NewRouter()
+  r.Use(loggingMiddleware)
+
   r.HandleFunc("/identity/login", handler.Login).Methods("POST")
   r.HandleFunc("/identity/register", handler.Register).Methods("POST")
 
