657dd1f77ddde1b350a10daa770986a7561fb00f
Dronicode <luffy.obrien@outlook.com>
2025-09-18 17:12:19 +0200

    Added test util for passing .env into other tests easily

---
+++ backend/identity/pkg/testutil/env.go
@@ -0,0 +1,25 @@
+package testutil
+
+import (
+	"log"
+	"os"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
+var loadOnce sync.Once
+
+func Env(key string) string {
+	loadOnce.Do(func() {
+		if err := godotenv.Load("../../.env"); err != nil {
+			log.Printf("Warning: .env failed to load: %v", err)
+		}
+	})
+
+	val := os.Getenv(key)
+	if val == "" {
+		log.Printf("Warning: env var %s is not set", key)
+	}
+	return val
+}
